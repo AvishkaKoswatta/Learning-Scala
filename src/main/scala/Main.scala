8dc2e8fd6f2f2710e15920aa32ea3e0f6fb8fd48
Avishka <avishkakkoswaththa@gmail.com>
2025-03-12 20:07:02 +0530

    variable declaration

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
-@main def hello(): Unit =
+import scala.io.StdIn 
+@main def hello(): Unit = //entry point to the program
   println("Hello world!")
   println(msg)
+  println("Enter name: ")
+  val name = StdIn.readLine()
+  println(s"Hello, $name") //s is used to interpolate variables in a string
 
 def msg = "I was compiled by Scala 3. :)"
+
+var X:Int=10
+val Y:String="Hello"
