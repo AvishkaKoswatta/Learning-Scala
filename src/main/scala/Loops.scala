c4755eaecff8677e253889aed058df2e2013c007
Avishka <avishkakkoswaththa@gmail.com>
2025-03-19 21:41:05 +0530

    add examples of yield in Scala

--- src/main/scala/Loops.scala
+++ src/main/scala/Loops.scala
@@ -1,12 +1,14 @@
 import scala.compiletime.ops.double
 object LoopMain {
   def main(args: Array[String]): Unit = {
+
     //while
     var x=0
     while (x<5) { 
         x=x+1
         println(x)
     }
+
     //simple for loop
     var i=0
     for (i <- 1 to 5){
@@ -15,6 +17,7 @@ object LoopMain {
     for (i <- 1 until 6){
         println(i)
     }
+
     //multipe range
     var j=0
     for (i <- 1 to 3; j <- 1 to 5){
@@ -27,11 +30,20 @@ object LoopMain {
     for (m <- list1){
       println(m)
     }
+
     //with conditions
     for(m <- list1
     if m!=2; if m < 7){
         println(m)
     }
+
+    //Yield 
+    val numList=List(1,2,3,4,5)
+    val retVal = for {x <- numList if x < 4; if x!=3}yield x
+    for (x <- retVal){
+      println(x)
+    }
+
 
   }
 }
