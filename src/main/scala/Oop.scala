40cb929a7ac8e9465f813c22eb0b0dd694092ed7
Avishka <avishkakkoswaththa@gmail.com>
2025-03-21 17:57:35 +0530

    add inheritance example with Employee class extending Person

--- src/main/scala/Oop.scala
+++ src/main/scala/Oop.scala
@@ -1,17 +1,27 @@
 object OopMain {
   def main(args: Array[String]): Unit = {
-
+    //classes & objects
     class Person(name:String, year:Int){
         val Name:String=name
         val Year:Int=year
 
         def celebrateBirthday(): Unit=
-            var age:Int=2025-Year
-            var wish=println(s"Happy $age birthday $name !")
+            val age:Int=2025-Year
+            println(s"Happy $age birthday $name !")
         
     }
     val person=Person("Alice", 2000)
     person.celebrateBirthday()
+
+    //Inheritance
+    class Employee(name:String, year:Int, role:String) extends Person(name:String, year:Int){
+      override def celebrateBirthday(): Unit = 
+        val age:Int=2025-Year
+        println(s"Happy $age birthday $name !. You are a great $role")
+    }
+    
+    val employee =new Employee("Bob",1995, "SE")
+    employee.celebrateBirthday()
 
   }
 }
