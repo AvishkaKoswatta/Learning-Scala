8f37eab40291020369edd92bb8ed1a16c510f7ff
Avishka <avishkakkoswaththa@gmail.com>
2025-03-26 09:29:38 +0530

    add case class Person with value comparison and copy method examples

--- src/main/scala/Implicit.scala
+++ src/main/scala/Implicit.scala
@@ -8,6 +8,21 @@ object IntExtensions{
     println(3.isEven())
     // like in built methods for int, string etc wecan add our own methods to them using implicit classes
 
+
+    //case class
+    case class Person(name:String, age:Int)
+
+    val p1=Person("alice", 20) //No need to use new when creating an object here
+    println(p1.name)
+
+    val p2=Person("bob",30)
+    println(p1==p2) //here compare values instead of object references. Other classes it compare objects in memory. 
+    //If two different objects have same memory and age, they give false beacuse two objects. 
+    //But in scala will give true as it compare values.
+
+    val p3=p1.copy(age=21) //creates a new object with some modified values
+    println(p3)
+
 }
 }
 
